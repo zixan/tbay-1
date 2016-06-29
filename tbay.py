bcc0bccba3294baf87ff7314a7dfc86b2e157df8
Zeeshan Mughal <zeeshan.jp@gmail.com>
2016-06-29 12:23:17 -0400

    Fix relationship bugs

--- tbay.py
+++ tbay.py
@@ -9,28 +9,28 @@ Base = declarative_base()
 
 from datetime import datetime
 
-from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, Float
 from sqlalchemy.orm import relationship
 
-user_bid_table = Table('user_bid_association', Base.metadata,
-    Column('user_id', Integer, ForeignKey('user.id')),
-    Column('bid_id', Integer, ForeignKey('bid.id'))
-)
+# user_bid_table = Table('user_bid_association', Base.metadata,
+#     Column('user_id', Integer, ForeignKey('user.id')),
+#     Column('bid_id', Integer, ForeignKey('bid.id'))
+# )
 
-bid_item_table = Table('bid_item_association', Base.metadata,
-    Column('bid_id', Integer, ForeignKey('bid.id')),
-    Column('item_id', Integer, ForeignKey('item.id'))
-)
+# bid_item_table = Table('bid_item_association', Base.metadata,
+#     Column('bid_id', Integer, ForeignKey('bid.id')),
+#     Column('item_id', Integer, ForeignKey('item.id'))
+# )
 
 class Item(Base):
-    __tablename__ = "item"
+    __tablename__ = "items"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
-    
-    seller_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    bids = relationship("Bid", backref="items")
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 class User(Base):
     __tablename__ = "user"
@@ -38,29 +38,32 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    items = relationship("Item", backref="seller")
-    bids = relationship("Bid", secondary="user_bid_association",
-                            backref="userbid")
+    items = relationship("Item", backref="user")
+    bids = relationship("Bid", backref="user")
 
 class Bid(Base):
     __tablename__ = "bid"
 
     id = Column(Integer, primary_key=True)
-    floating_price = Column(Integer, nullable=False)
-    items = relationship("Item", secondary="bid_item_association",
-                            backref="biditem")
+    floating_price = Column(Float, nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
 
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
-baseball = Item(name="baseball", description="Yankees Baseball for Sale")
-beyonce = User(username="queenbey", password="crazyinlove", items=[baseball])
+beyonce = User(username="queenbey", password="crazyinlove")
 jayz = User(username="jayznyc", password="hovahova")
 solange = User(username="sistersol", password="lilknowles")
 
+baseball = Item(name="baseball", description="Yankees Baseball for Sale", user=beyonce)
+beyonce.items.append(baseball)
 
-session.add_all([baseball, beyonce, jayz, solange])
+bid1 = Bid(floating_price=11.0, items=baseball, user=jayz)
+bid2 = Bid(floating_price=20.0, items=baseball, user=solange)
+
+session.add_all([beyonce, jayz, solange, baseball, bid1, bid2])
 # session.add_all([ jayz, solange])
 session.commit()
 
@@ -70,6 +73,9 @@ for user in session.query(User).all():
     print user.username
     print user.password
     print user.id
+    for bid in user.bids:
+        print bid.floating_price
+    
 
 # beyonce = User()
 # beyonce.username = "bknowles"
